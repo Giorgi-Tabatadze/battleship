375ac30ad9f5076307d2ddc0c1b2b88498cc9cdc
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-07-20 13:53:15 +0400

    attacks sunk ships and mark sunk ships on board

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -4,21 +4,23 @@ import Ship from "../ship/ship";
 
 const Gameboard = function () {
   // generates a (10x10) board where we can create and place ships
-
   const generateBoard = () => {
     const board = {};
     for (let i = 0; i < 100; i++) {
-      const isAttacked = false;
+      const hasBeenAttacked = false;
       const shipThere = false;
       const sunkShipThere = false;
-      board[i] = { isAttacked, shipThere, sunkShipThere };
+      board[i] = { hasBeenAttacked, shipThere, sunkShipThere };
     }
     return board;
   };
 
   const spaceIsAvailable = function (coordinates) {
     for (const coordinate of coordinates) {
-      if (this.board[coordinate].shipThere) {
+      if (
+        this.board[coordinate].shipThere ||
+        this.board[coordinate].shipThere === 0
+      ) {
         return false;
       }
     }
@@ -29,17 +31,58 @@ const Gameboard = function () {
     // build a ship
     const ship = Ship(coordinates);
 
-    // put ship onto the board
-    coordinates.forEach((coordinate) => {
-      this.board[coordinate].shipThere = true;
-    });
     // put ship into tracking array
     this.ships.push(ship);
+
+    // put ship onto the board and indicate which ship occupies the space
+    coordinates.forEach((coordinate) => {
+      this.board[coordinate].shipThere = this.ships.indexOf(ship);
+    });
+  };
+
+  const spaceAttackedBefore = function (coordinate) {
+    return this.board[coordinate].hasBeenAttacked;
+  };
+
+  const attack = function (coordinate) {
+    this.board[coordinate].hasBeenAttacked = true;
+    if (
+      this.board[coordinate].shipThere ||
+      this.board[coordinate].shipThere === 0
+    ) {
+      const shipId = this.board[coordinate].shipThere;
+      this.ships[shipId].coordinates[coordinate] = true;
+      // if ship was hit return ship id
+      return shipId;
+    }
+    // esle return false
+    return false;
+  };
+
+  const shipSunk = function (shipId) {
+    this.ships[shipId].sunk = this.ships[shipId].isSunk();
+    // mark sunk ship on the board.
+    if (this.ships[shipId].sunk) {
+      // eslint-disable-next-line guard-for-in
+      for (const coordinate in this.ships[shipId].coordinates) {
+        this.board[coordinate].sunkShipThere = true;
+      }
+    }
+    // return that ship was sunk
+    return this.ships[shipId].sunk;
   };
 
   const board = generateBoard();
   const ships = [];
 
-  return { board, ships, createShip, spaceIsAvailable };
+  return {
+    board,
+    ships,
+    createShip,
+    spaceIsAvailable,
+    spaceAttackedBefore,
+    attack,
+    shipSunk,
+  };
 };
 export default Gameboard;
--- src/gameboard/gameboard.test.js
+++ src/gameboard/gameboard.test.js
@@ -10,7 +10,7 @@ it("generates board with correct values", () => {
   });
   for (let i = 0; i < Object.keys(gameBoard.board).length; i++) {
     expect(mockCallback.mock.results[i].value).toStrictEqual({
-      isAttacked: false,
+      hasBeenAttacked: false,
       shipThere: false,
       sunkShipThere: false,
     });
@@ -25,13 +25,13 @@ it("ships are placed correctly oth the board", () => {
   const coordinates = [41, 42];
   gameBoard.createShip(coordinates);
   expect(gameBoard.board[41]).toStrictEqual({
-    isAttacked: false,
-    shipThere: true,
+    hasBeenAttacked: false,
+    shipThere: 0,
     sunkShipThere: false,
   });
   expect(gameBoard.board[42]).toStrictEqual({
-    isAttacked: false,
-    shipThere: true,
+    hasBeenAttacked: false,
+    shipThere: 0,
     sunkShipThere: false,
   });
   const mockCallback = jest.fn((x) => x);
@@ -43,7 +43,7 @@ it("ships are placed correctly oth the board", () => {
       continue;
     }
     expect(mockCallback.mock.results[i].value).toStrictEqual({
-      isAttacked: false,
+      hasBeenAttacked: false,
       shipThere: false,
       sunkShipThere: false,
     });
@@ -75,4 +75,92 @@ it("check if space is available in unoccupied space", () => {
   const coordinates = [41, 42, 43];
   gameBoard.createShip(coordinates);
   expect(gameBoard.spaceIsAvailable([53, 56, 35])).toBe(true);
+});
+it("verify if coordinate has been shot at before", () => {
+  const gameBoard = Gameboard();
+  const coordinates = [41, 42, 43];
+  gameBoard.createShip(coordinates);
+  gameBoard.board[42].hasBeenAttacked = true;
+  expect(gameBoard.spaceAttackedBefore(42)).toBe(true);
+});
+it("verify if coordinate has not been shot at before", () => {
+  const gameBoard = Gameboard();
+  const coordinates = [41, 42, 43];
+  gameBoard.createShip(coordinates);
+  gameBoard.board[42].hasBeenAttacked = true;
+  expect(gameBoard.spaceAttackedBefore(62)).toBe(false);
+});
+it("shot registered on the board", () => {
+  const gameBoard = Gameboard();
+  gameBoard.attack(45);
+  expect(gameBoard.board[45].hasBeenAttacked).toBe(true);
+  Object.keys(gameBoard.board).forEach((coordinate) => {
+    if (coordinate == 45) {
+      return;
+    }
+    expect(gameBoard.board[coordinate].hasBeenAttacked).toBe(false);
+  });
+});
+it("attack registered on the ship", () => {
+  const gameBoard = Gameboard();
+  const coordinates = [41, 42, 43];
+  gameBoard.createShip(coordinates);
+  gameBoard.attack(42);
+  expect(gameBoard.ships[0].coordinates[41]).toBe(false);
+  expect(gameBoard.ships[0].coordinates[42]).toBe(true);
+  expect(gameBoard.ships[0].coordinates[43]).toBe(false);
+});
+it("check if ship was sunk successfuly", () => {
+  const gameBoard = Gameboard();
+  const coordinates = [41, 42, 43];
+  gameBoard.createShip(coordinates);
+  const attack40 = gameBoard.attack(40);
+  console.log(attack40);
+  if (attack40 || attack40 === 0) {
+    expect(gameBoard.shipSunk(attack40)).toBe(false);
+  }
+  const attack41 = gameBoard.attack(41);
+  if (attack41 || attack41 === 0) {
+    expect(gameBoard.shipSunk(attack41)).toBe(false);
+  }
+  const attack42 = gameBoard.attack(42);
+  if (attack42 || attack42 === 0) {
+    expect(gameBoard.shipSunk(attack42)).toBe(false);
+  }
+  const attack43 = gameBoard.attack(43);
+  if (attack43 || attack43 === 0) {
+    expect(gameBoard.shipSunk(attack43)).toBe(true);
+  }
+});
+it("ship marked sunk on the board", () => {
+  const gameBoard = Gameboard();
+  const coordinates = [41, 42, 43];
+  gameBoard.createShip(coordinates);
+  const attack40 = gameBoard.attack(40);
+  console.log(attack40);
+  if (attack40 || attack40 === 0) {
+    expect(gameBoard.board[41].sunkShipThere).toBe(false);
+    expect(gameBoard.board[42].sunkShipThere).toBe(false);
+    expect(gameBoard.board[43].sunkShipThere).toBe(false);
+  }
+  const attack41 = gameBoard.attack(41);
+  if (attack41 || attack41 === 0) {
+    expect(gameBoard.board[41].sunkShipThere).toBe(false);
+    expect(gameBoard.board[42].sunkShipThere).toBe(false);
+    expect(gameBoard.board[43].sunkShipThere).toBe(false);
+  }
+  const attack42 = gameBoard.attack(42);
+  if (attack42 || attack42 === 0) {
+    expect(gameBoard.board[41].sunkShipThere).toBe(false);
+    expect(gameBoard.board[42].sunkShipThere).toBe(false);
+    expect(gameBoard.board[43].sunkShipThere).toBe(false);
+  }
+  const attack43 = gameBoard.attack(43);
+  if (attack43 || attack43 === 0) {
+    if (gameBoard.shipSunk(attack43)) {
+      expect(gameBoard.board[41].sunkShipThere).toBe(true);
+      expect(gameBoard.board[42].sunkShipThere).toBe(true);
+      expect(gameBoard.board[43].sunkShipThere).toBe(true);
+    }
+  }
 });
